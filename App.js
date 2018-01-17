588bc934823bf7aa789d4efd03d44d702ecfd256
Admir Ošijan <osijan.admir@gmail.com>
2018-01-17 23:45:16 +0100

    add file

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { } from 'react-native';
+import Apps from './src/Apps'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Apps />
+    );
+  }
+}
+
+
